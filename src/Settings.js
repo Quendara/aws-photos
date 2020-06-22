79ebba72bb56a2ad5c6914a44fb548811f43d3cb
agarstka <andregarstka@web.de>
2020-06-22 14:40:25 +0200

    Fix API URL

--- src/Settings.js
+++ src/Settings.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Settings = {
  
-  baseRestApi : "https://srxdhyyhm2.execute-api.eu-central-1.amazonaws.com/dev/",
+  baseRestApi : "https://srxdhyyhm2.execute-api.eu-central-1.amazonaws.com/dev",
   baseS3Bucket : "https://s3.eu-central-1.amazonaws.com//quendara-images-thumbs/"
- 
+
 } 
 
 export default Settings;
