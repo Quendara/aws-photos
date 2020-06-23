a4eb6005f6d11c4b50802cdf3f9040200fd3f803
agarstka <andregarstka@web.de>
2020-06-23 21:57:04 +0200

    Move to top

---
+++ src/components/MoveToTop.js
@@ -0,0 +1,49 @@
+
+import React, { useState, useCallback, useEffect, useRef } from "react";
+import { Icon } from "./Icons";
+import { setPhotos } from "../redux/actions";
+
+export const MoveToTop = ( ) => {
+
+    const [is_visible, setVisible] = useState(false);
+
+    useEffect(() => {
+        document.addEventListener("scroll", toggleVisibility );
+
+        return () => {
+            document.removeEventListener("scroll", toggleVisibility );
+        };
+    });    
+    
+
+    const scrollToTop = () => {
+        window.scrollTo({
+          top: 0,
+          behavior: "smooth"
+        });
+      }      
+
+    const toggleVisibility = () => {
+        if (window.pageYOffset > 300) {
+            setVisible( true )
+        } else {
+            setVisible( false )
+        }
+      }
+    
+    const getClassName = ( is_visible ) => {
+        if( is_visible ){
+            return "btn-move-to-top btn btn-large blue"
+        }
+        return "btn-move-to-top invisible btn btn-large blue"
+
+    }
+
+    return (
+        <>
+            <a className={ getClassName( is_visible )} onClick={ scrollToTop }><Icon icon="arrowUp" /></a>
+        </>
+    )
+}
+
+
