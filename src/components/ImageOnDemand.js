9defe27db1aafeda582eab16ecba8c7cf60e6001
agarstka <andregarstka@web.de>
2020-08-21 09:27:48 +0200

    Keep images loaded

--- src/components/ImageOnDemand.js
+++ src/components/ImageOnDemand.js
@@ -1,45 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
 import { useVisible } from 'react-hooks-visible'
 
 
 // className={isVisible ? 'excited' : ''}
- // 
+// 
 export const ImageOnDemand = ({ image, className, onClick }) => {
-    // Boolean. This example is 50% visible.
-    // const [targetRef, visible] = useVisible()
-    // Boolean. This example is 50% visible.
-    // const [targetRef, isVisible] = useVisible((vi: number) => vi > 0.02)
-    const [targetRef, isVisible] = useVisible((vi) => vi > 0.01)
-    let loaded = false;
+  // Boolean. This example is 50% visible.
+  // const [targetRef, visible] = useVisible()
+  // Boolean. This example is 50% visible.
+  // const [targetRef, isVisible] = useVisible((vi: number) => vi > 0.02)
 
-    const getSrcUrlWhenVisible = (isVisible) => {
-        if( isVisible || loaded ){
-          loaded = true          
-          console.log( "LOADED : ", image.source_url, isVisible, loaded )          
-          return image.source_url;          
-        }
-        // console.log( "EMPTY : ", image.source_url, isVisible, loaded )
-        return ""
-    }
+  // Percent value.
+  // const [targetRef, visibility] = useVisible()    
+  const [isVisibleState, setVisible] = useState(false)
+  const [loaded, setLoaded] = useState(false)
 
-    const getClassName = (image, className) => {
+  const [targetRef, visibility] = useVisible((vi) => vi)
 
-      let r = className
+  // let loaded = false;
 
-      if( image.rotate !== undefined ){
-        if( image.rotate === 180 ){
-          r += " rotate180"
-        }
+  // const getSrcUrlWhenVisible = (isVisible) => {
+  //     if( isVisible || loaded ){
+  //       loaded = true          
+  //       console.log( "LOADED : ", image.source_url, isVisible, loaded )          
+  //       return image.source_url;          
+  //     }
+  //     // console.log( "EMPTY : ", image.source_url, isVisible, loaded )
+  //     return ""
+  // }
+
+  const getClassName = (image, className, visibility) => {
+
+    if (visibility > 0.01) {
+      if (loaded === false) {
+        setLoaded(true)
+        console.log("set loaded TRUE")
       }
-      return r      
     }
+    let r = className
 
-    
-
-    // <img ref={targetRef} className={className} src={ isVisible ? image.source_url : ''} />  
-    // <img ref={targetRef} className={className} src={ getSrcUrlWhenVisible( isVisible, loaded ) } />
-    return (
-        <img ref={targetRef} className={ getClassName( image, className ) } onClick={onClick} src={ isVisible ? image.source_url : ''} />
-    )
+    if (image.rotate !== undefined) {
+      if (image.rotate === 180) {
+        r += " rotate180"
+      }
+    }
+    return r
   }
-    
+
+
+
+  // <img ref={targetRef} className={className} src={ isVisible ? image.source_url : ''} />  
+  // <img ref={targetRef} className={className} src={ getSrcUrlWhenVisible( isVisible, loaded ) } />
+  return (
+
+    <>
+      <img ref={ targetRef } alt={ visibility } className={ getClassName(image, className, visibility) } onClick={ onClick } src={ ((visibility > 0.01) || loaded) ? image.source_url : '' } />
+      {/* <div  className={ getClassName( image, className, visibility ) }> x {visibility}</div> */ }
+    </>
+
+  )
+}
