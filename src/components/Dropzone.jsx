6f407836ddd726ef817d90abaf1e7d859b6bb556
agarstka <andregarstka@web.de>
2020-11-06 18:43:47 +0100

    Upload is working

--- src/components/Dropzone.jsx
+++ src/components/Dropzone.jsx
@@ -1,8 +1,8 @@
-import React, {useMemo} from 'react';
+import React, { useMemo } from 'react';
 
-import axios from 'axios'; 
-import {useDropzone} from 'react-dropzone';
-import {Settings} from '../Settings';
+import axios from 'axios';
+import { useDropzone } from 'react-dropzone';
+import { Settings } from '../Settings';
 
 
 const baseStyle = {
@@ -42,7 +42,7 @@ export function Dropzone(props) {
     isDragActive,
     isDragAccept,
     isDragReject
-  } = useDropzone({accept: 'image/*', onDrop: files => onDrop(files) });
+  } = useDropzone({ accept: 'image/*', onDrop: files => onDrop(files) });
 
   const style = useMemo(() => ({
     ...baseStyle,
@@ -55,61 +55,96 @@ export function Dropzone(props) {
     isDragAccept
   ]);
 
+  // export const uploadToAws = async (signedRequest, file) => {
+  //   const base64 = await fs.readFile(file.uri, 'base64')
+  //   const buffer = Buffer.from(base64, 'base64')
+  //   return fetch(signedRequest, {
+  //     method: 'PUT',
+  //     headers: {
+  //     'Content-Type': 'image/jpeg; charset=utf-8',
+  //     'x-amz-acl': 'public-read',
+  //    },
+  //     body: buffer,
+  //   })
+  // }      
+
   function onDrop(acceptedFiles) {
-    
+
     // const req = request.post('/upload')
     acceptedFiles.forEach(file => {
       // req.attach(file.name, file)
-      const formData = new FormData(); 
-     
-      // Update the formData object 
-      formData.append( 
-        "myFile", 
-        file,
-        file.name 
-      ); 
-     
-      // Details of the uploaded file 
-      console.log( file ); 
-     
+      // const formData = new FormData();
+
+      // // Update the formData object 
+      // formData.append(
+      //   "myFile",
+      //   file,
+      //   file.name
+      // );
+
+      // // Details of the uploaded file 
+      // console.log(file);
+
       // Request made to the backend api 
       // Send formData object 
-      const url = [ Settings.baseRestApi, "uploadfile" ].join("/")
-      axios.post( url, formData); 
+      const url = [Settings.baseRestApi, "photoData", "upload"].join("/")
+      // axios.post( url, formData );
 
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type", "image/jpeg");
+      
+      var file2 = "<file contents here>";
+      
+      var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: file,
+        redirect: 'follow'
+      };
+      
+      fetch("https://srxdhyyhm2.execute-api.eu-central-1.amazonaws.com/dev/photoData/upload", requestOptions)
+        .then(response => response.text())
+        .then(result => console.log(result))
+        .catch(error => console.log('error', error));
 
+      // axios({
+      //   method: "POST",
+      //   url: url,
+      //   data: file,
+      //   headers: { "Content-Type": "image/jpeg" }
+      // })
     })
     // req.end(callback)
-  }  
+  }
 
   const acceptedFileItems = acceptedFiles.map(file => (
-    <li key={file.path}>
-      {file.path} - {file.size} bytes
+    <li key={ file.path }>
+      {file.path } - {file.size } bytes
     </li>
   ));
 
   const fileRejectionItems = fileRejections.map(({ file, errors }) => (
-    <li key={file.path}>
-      {file.path} - {file.size} bytes
+    <li key={ file.path }>
+      {file.path } - {file.size } bytes
       <ul>
-        {errors.map(e => (
-          <li key={e.code}>{e.message}</li>
-        ))}
+        { errors.map(e => (
+          <li key={ e.code }>{ e.message }</li>
+        )) }
       </ul>
-    </li> ));
+    </li>));
 
   return (
     <div className="container">
-      <div {...getRootProps({style})}>
-        <input {...getInputProps()} />
+      <div { ...getRootProps({ style }) }>
+        <input { ...getInputProps() } />
         <p>Drag 'n' drop some files here, or click to select files</p>
       </div>
       <aside>
         <h4>Accepted files</h4>
-        <ul>{acceptedFileItems}</ul>
+        <ul>{ acceptedFileItems }</ul>
         <h4>Rejected files</h4>
-        <ul>{fileRejectionItems}</ul>
-      </aside>    
+        <ul>{ fileRejectionItems }</ul>
+      </aside>
     </div>
   );
 }
